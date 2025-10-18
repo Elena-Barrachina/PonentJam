f2875e9d50294c8be887b56f8ab8d4730c739b8e
Elena Barrachina i Ribes <ebarrachina2.olivera@gmail.com>
2025-10-18 11:32:19 +0200

    Base nivel 1
    El personaje anda, la gravedad funciona, el suelo está congelado

--- scenes/jugador.gd
+++ scenes/jugador.gd
@@ -9,14 +9,14 @@ const GRAVITY = 1000
 func _physics_process(delta):
 	var input_dir = 0
 
-	if Input.is_action_pressed("ui_right") or Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("ui_right"):
 		input_dir += 1
-	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("ui_left"):
 		input_dir -= 1
 
 	velocity.x = input_dir * SPEED
 
-	if (Input.is_action_just_pressed("ui_accept") or Input.is_action_just_pressed("move_jump")) and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
 
 	if not is_on_floor():
