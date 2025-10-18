06027ca8c93140677eaf5a4183ceb6fc6d44a6b4
Elena Barrachina i Ribes <ebarrachina2.olivera@gmail.com>
2025-10-18 19:11:44 +0200

    Coleccionables
    Ahora se pueden recoger llamas (instancias de clas... de escenas).

--- scripts/jugador.gd
+++ scripts/jugador.gd
@@ -5,6 +5,9 @@ const JUMP_FORCE = -400
 const GRAVITY = 1000
 
 @onready var anim = $AnimatedSprite2D
+@onready var counter_label = get_tree().get_first_node_in_group("ui_counter")
+
+var collectibles = 0
 
 func _physics_process(delta):
 	var input_dir = 0
@@ -31,3 +34,19 @@ func _physics_process(delta):
 			anim.flip_h = input_dir < 0
 		else:
 			anim.animation = "Parado"
+
+func _ready():
+	add_to_group("player")  # This allows collectibles to recognize this as the player
+
+func add_collectible():
+	collectibles += 1
+	update_ui()
+	
+func update_ui():
+	if counter_label:
+		counter_label.text = str(collectibles)
+	print("Collectibles: ", collectibles)
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("Llama"):
+		print("collected flame")
